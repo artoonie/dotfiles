81eb1694e106486028592eb041639b99801722c6
Armin Samii <asamii@adobe.com>
2015-11-09 16:43:06 -0800

    Imagemagick, slack, lolcommits

--- .new-machine-installation.sh
+++ .new-machine-installation.sh
@@ -16,6 +16,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # Install things with homebrew
 brew install tmux
 brew install rmtrash
+brew install imagemagick
 
 # Install pathogen for vim plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -23,5 +24,10 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vi
 # Install vim plugins
 git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/vim-easymotion
 
+# Install lolcommits
+sudo gem install lolcommits
+
 # Notes to self
-echo "Now install BetterSnapTool from the AppStore"
+echo "Now install BetterSnapTool and Slack from the AppStore"
+echo "Then install iStat Menus from https://bjango.com/mac/istatmenus/"
+echo "Then install Creative Cloud https://creative.adobe.com/products/download/photoshop"
